06eb93cdf6f86a2fed7311d3ab1ea489830b696e
Izukend <98744675+Izukend@users.noreply.github.com>
2024-05-04 21:02:37 +0200

    04/05/24 Update wifi_and_mqtt.cpp

--- src/traitements/wifi_and_mqtt.cpp
+++ src/traitements/wifi_and_mqtt.cpp
@@ -1,24 +1,24 @@
 /**
  * @file wifi_and_mqtt.cpp
- * @brief Gestion de la connexion WiFi et MQTT
+ * @brief WiFi and MQTT Connection Management
  * @date 2024-04-05
- * @author Tom
+ * @author Izukend
  */
 
-#include "../../include/wifi_and_mqtt.h" // Inclure les bibliothèques nécessaires
+#include "../../include/wifi_and_mqtt.h" // Include necessary libraries
 
-WebServer server(80); // Créer une instance de serveur Web sur le port 80
-WiFiClient eClient; // Créer une instance de client WiFi
-PubSubClient client(eClient); // Créer une instance de client MQTT
+WebServer server(80); // Create an instance of Web server on port 80
+WiFiClient eClient; // Create an instance of WiFi client
+PubSubClient client(eClient); // Create an instance of MQTT client
 
 /**
- * @brief Fonction pour gérer la soumission du formulaire
+ * @brief Function to handle form submission
  * 
- * Cette fonction récupère les informations du formulaire, tente de se connecter au réseau WiFi
- * et au serveur MQTT, puis envoie un message MQTT.
+ * This function retrieves form information, attempts to connect to the WiFi network
+ * and the MQTT server, and then sends an MQTT message.
  */
 void handleFormSubmit() {
-  // Récupérer les informations du formulaire
+  // Retrieve form information
   String esp32Name = server.arg("name");
   String wifiSSID = server.arg("name_wifi");
   String wifiPassword = server.arg("pass");
@@ -26,82 +26,82 @@ void handleFormSubmit() {
   String mqttUser = server.arg("user");
   String mqttPassword = server.arg("pass_mqtt");
   
-  Serial.print("Tentative de connexion au réseau WiFi: ");
+  Serial.print("Attempting to connect to WiFi network: ");
   Serial.println(wifiSSID);
 
   WiFi.begin(wifiSSID.c_str(), wifiPassword.c_str());
 
-  // Attendre la connexion au réseau WiFi
+  // Wait for WiFi network connection
   if (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print("Veuillez configurer à nouveau le système");
+    Serial.print("Please reconfigure the system");
   } else{
-    Serial.print("Connexion au WIFI choisi effectuée avec succès");
+    Serial.print("Successfully connected to chosen WIFI");
   }
 
-  Serial.print("Tentative de connexion au serveur MQTT: ");
+  Serial.print("Attempting to connect to MQTT server: ");
   client.setServer(mqttServer.c_str(), 1883);
   
-  // Tenter de se connecter au serveur MQTT
+  // Attempt to connect to MQTT server
   if (client.connect("ESP32Client", mqttUser.c_str(), mqttPassword.c_str())) {
     
-    Serial.println("Connecté au MQTT !");
+    Serial.println("Connected to MQTT!");
 
-    WiFi.softAPdisconnect(false); // Désactiver le mode station WiFi
-    Serial.print("WiFi déconnecté");
+    WiFi.softAPdisconnect(false); // Disable WiFi station mode
+    Serial.print("WiFi disconnected");
     delay(1000);
 
-    subcribeTopic(); // S'abonner au topic MQTT
-    client.setCallback(callback); // Définir la fonction de rappel MQTT
+    subcribeTopic(); // Subscribe to MQTT topic
+    client.setCallback(callback); // Set MQTT callback function
 
   } else {
-    Serial.print("Échec de la connexion, erreur: ");
+    Serial.print("Connection failed, error: ");
     Serial.println(client.state());
   } 
-  oledErase(); // Effacer l'écran OLED
+  oledErase(); // Clear OLED screen
 }
 
 /**
- * @brief Fonction pour s'abonner à un topic MQTT
+ * @brief Function to subscribe to an MQTT topic
  * 
- * Cette fonction permet de s'abonner au topic "topic/capteurs".
+ * This function subscribes to the "topic/sensors" MQTT topic.
  */
 void subcribeTopic(){
-    // S'abonner au topic MQTT distant
-  client.subscribe("topic/capteurs");
-  Serial.println("Abonnement au topic distant effectué avec succès !");
+    // Subscribe to remote MQTT topic
+  client.subscribe("topic/sensors");
+  Serial.println("Subscribed to remote topic successfully!");
 }
 
 /**
- * @brief Fonction pour envoyer un message MQTT avec une trame JSON
+ * @brief Function to send an MQTT message with a JSON frame
  * 
- * Cette fonction convertit une chaîne JSON en un tableau de caractères et l'envoie sur un topic MQTT désiré.
+ * This function converts a JSON string to a character array and sends it on a desired MQTT topic.
  * 
- * @param message La chaîne JSON à envoyer
+ * @param message The JSON string to send
  */
 void sendMQTTFrame(const String& message) {
-  // Convertir la chaîne JSON en tableau de caractères pour l'envoi MQTT
+  // Convert JSON string to character array for MQTT sending
   char messageBuffer[200];
   message.toCharArray(messageBuffer, 200);
 
-  // Envoyer le message MQTT sur le topic désiré
-  client.publish("topic/capteurs", messageBuffer);
-  Serial.println("Message MQTT envoyé avec succès !");
+  // Send MQTT message on desired topic
+  client.publish("topic/sensors", messageBuffer);
+  Serial.println("MQTT message sent successfully!");
   Serial.println(messageBuffer);
 }
 
 /**
- * @brief Fonction de rappel pour la réception de messages MQTT
+ * @brief Callback function for receiving MQTT messages
  * 
- * Cette fonction est appelée lorsque des messages sont reçus sur un topic MQTT auquel le client est abonné.
- * Elle affiche le topic et le contenu du message sur la console série.
- * @param Le topic, le contenue du message et la longueur du message
+ * This function is called when messages are received on an MQTT topic to which the client is subscribed.
+ * It displays the topic and message content on the serial console.
+ * @param topic The topic, message content, and message length
  */
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message reçu sur le topic : ");
+  Serial.print("Message received on topic: ");
   Serial.println(topic);
 
-  Serial.print("Contenu du message : ");
+  Serial.print("Message content: ");
   for (int i = 0; i < length; i++) {
       Serial.print((char)payload[i]);
   }
